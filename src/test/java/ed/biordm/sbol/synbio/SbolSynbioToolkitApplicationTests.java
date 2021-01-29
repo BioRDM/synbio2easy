6185203ab57e020006524fa02758a94942f2ed12
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2021-01-29 14:17:40 +0000

    Bare SB project

---
+++ src/test/java/ed/biordm/sbol/synbio/SbolSynbioToolkitApplicationTests.java
@@ -0,0 +1,13 @@
+package ed.biordm.sbol.synbio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbolSynbioToolkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
