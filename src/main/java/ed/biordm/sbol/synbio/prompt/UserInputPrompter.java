cb7d94aff94a416d77ae8748efccc0df21cc5ff0
hayjohnny2000 <hayjohnny2000@yahoo.com>
2021-02-11 10:33:47 +0000

    Add additional CLI params to UserInputPrompter

--- src/main/java/ed/biordm/sbol/synbio/prompt/UserInputPrompter.java
+++ src/main/java/ed/biordm/sbol/synbio/prompt/UserInputPrompter.java
@@ -24,24 +24,24 @@ import org.springframework.stereotype.Component;
 public class UserInputPrompter {
 
     final Console console;
-    
+
     public UserInputPrompter() {
         this(System.console());
-        
+
     }
-    
+
     // for testing so that console can be mocked
     protected UserInputPrompter(Console console) {
         this.console = console;
     }
-    
+
     public CommandOptions getCommandOptions(ApplicationArguments args) throws MissingOptionException {
-    
-        Command command = getCommand(args); 
-        
-        CommandOptions options = new CommandOptions(command);        
-        setPassedOptions(options, args);        
-        
+
+        Command command = getCommand(args);
+
+        CommandOptions options = new CommandOptions(command);
+        setPassedOptions(options, args);
+
         switch (command) {
             case DEPOSIT: return promptDepositOptions(options);
             default: throw new IllegalArgumentException("Unsupported command: "+command);
@@ -49,33 +49,33 @@ public class UserInputPrompter {
     }
 
     Command getCommand(ApplicationArguments args) throws MissingOptionException {
-        
+
         String command;
-        
+
         if (args.getNonOptionArgs().isEmpty()) {
             command = promptForCommand();
         } else {
             command = args.getNonOptionArgs().get(0);
         }
-        
+
         switch(command) {
             case "deposit": return DEPOSIT;
             default: throw new MissingOptionException("Uknown command "+command);
         }
     }
-    
+
     String promptForCommand() {
-        
+
         console.printf("What operation do you want to perform?%n");
         console.printf("Choose from: deposit | describe%n");
-        
+
         String command = console.readLine("Operation: ");
-        
+
         return command;
     }
 
     CommandOptions promptDepositOptions(CommandOptions options) {
-        
+
         console.printf("... depositing desings into SynBioHub%n");
 
         if (options.user == null) {
@@ -83,13 +83,38 @@ public class UserInputPrompter {
         } else {
             console.printf("Username: %s", options.user);
         }
-        
+
         if (options.password == null) {
             options.password = new String(console.readPassword("Please enter your SynBioHub password: "));
         } else {
             console.printf("Password: *****");
         }
-        
+
+        if (options.crateNew == true) {
+            if (options.collectionName == null) {
+                options.collectionName = new String(console.readLine("Please enter a name for the new collection: "));
+            } else {
+                console.printf("New collection name: %s", options.collectionName);
+            }
+            if (options.url == null) {
+                options.url = new String(console.readLine("Please enter the URL of the SynBioHub server: "));
+            } else {
+                console.printf("SynBioHub URL: %s", options.url);
+            }
+        } else {
+            if (options.url == null) {
+                options.url = new String(console.readLine("Please enter the URL for the existing collection: "));
+            } else {
+                console.printf("Collection URL: %s", options.url);
+            }
+        }
+
+        if (options.dir == null) {
+            options.dir = new String(console.readLine("Please enter the directory path to upload: "));
+        } else {
+            console.printf("Directory: %s", options.dir);
+        }
+
         return options;
     }
 
@@ -100,14 +125,14 @@ public class UserInputPrompter {
     }
 
     void setPassedOptions(CommandOptions options, ApplicationArguments args) {
-        
+
         if (args.getOptionNames().contains("username") && !args.getOptionValues("username").isEmpty()) {
             options.user = args.getOptionValues("username").get(0);
         }
         if (args.getOptionNames().contains("u") && !args.getOptionValues("u").isEmpty()) {
             options.user = args.getOptionValues("u").get(0);
         }
-        
+
     }
-    
+
 }
