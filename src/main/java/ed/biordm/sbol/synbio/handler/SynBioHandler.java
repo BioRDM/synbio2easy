6c6c04bdd24ee80c42588ed03c31aa6777d5ff7f
hayjohnny2000 <hayjohnny2000@yahoo.com>
2021-02-11 17:40:16 +0000

    Add break statement in switch clause in SynBioHandler.handle

--- src/main/java/ed/biordm/sbol/synbio/handler/SynBioHandler.java
+++ src/main/java/ed/biordm/sbol/synbio/handler/SynBioHandler.java
@@ -6,6 +6,7 @@
 package ed.biordm.sbol.synbio.handler;
 
 import ed.biordm.sbol.synbio.client.SynBioClient;
+import ed.biordm.sbol.synbio.dom.Command;
 import ed.biordm.sbol.synbio.dom.CommandOptions;
 import java.io.File;
 import java.io.IOException;
@@ -16,8 +17,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.slf4j.LoggerFactory;
@@ -30,76 +29,73 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SynBioHandler {
-    
+
     final SynBioClient client;
 
     final org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
-    
+
     @Autowired
     public SynBioHandler(SynBioClient client) {
        this.client = client; 
     }
 
     public void handle(CommandOptions command) throws URISyntaxException {
-        
         switch (command.command) {
-            case DEPOSIT: handleDeposit(command);
+            case DEPOSIT: handleDeposit(command); break;
             default: throw new IllegalArgumentException("Unsuported command: "+command.command);
         }
     }
 
     void handleDeposit(CommandOptions parameters) throws URISyntaxException {
-        
         if (parameters.sessionToken == null) {
             parameters.sessionToken = login(parameters);
         }
-        
+
         if (parameters.multipleCollections) {
             depositMultipleCollections(parameters);            
         } else {
             depositSingleCollection(parameters);
         }
     }
-    
+
     String login(CommandOptions parameters) throws URISyntaxException {
-        
         String url = client.hubFromUrl(parameters.url);
+
         return client.login(url,parameters.user, parameters.password);
     }
-    
+
 
     void depositMultipleCollections(CommandOptions orgParameters) {
-        
+
         String prefix = orgParameters.collectionName;
-        
+
         List<Path> subCollections = subfolders(orgParameters.dir);
-        
+
         for (Path col: subCollections) {
             String suffix = col.getFileName().toString();
             String name = prefix+"_"+suffix;
-            
+
             CommandOptions params = orgParameters.clone();
             params.collectionName = name;
             params.dir = col.toString();
             params.multipleCollections = false;
-            
+
             depositSingleCollection(params);
         }
     }
 
     void depositSingleCollection(CommandOptions parameters) {
-        
         String collectionUrl = parameters.url;
         if (parameters.crateNew) {
             // woudl be cool if new collection could be created with api or by empty depoist
             // the deposit code would be cleaner;
             collectionUrl = createNewCollection(parameters);
         }
-        
+
         List<Path> files = getFiles(parameters);
-        
+
         for (Path file: files) {
-            
+
             // some other params as needed by the API
             client.deposit(parameters.sessionToken, collectionUrl, file);
         }
@@ -108,9 +104,9 @@ public class SynBioHandler {
     String createNewCollection(CommandOptions parameters) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     List<Path> subfolders(String dir) {
-        
+
         try (Stream<Path> files = Files.list(Paths.get(dir))) {
             return  files.filter( f -> Files.isDirectory(f))
                     .collect(Collectors.toList());            
