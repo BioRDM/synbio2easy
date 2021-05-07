f9a1f68bce77c12b76a306ab244460060fba9e0e
jhay-epcc <j.hay@epcc.ed.ac.uk>
2021-05-07 09:14:12 +0100

    Initial attempt at SynBioHub-CLI.sh - needs tested on Ubuntu/Mac

---
+++ SynBioHub-CLI.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo ${machine}
+# JAVA_VERSION=`java -version 2>&1 | head -n 1 | cut -d\" -f 2`
+
+if  [ "$machine" = "Linux" ]; then
+	xterm -hold -e "java -jar SynBioHub-CLI.jar"
+elif [ "$machine" = "Mac" ]; then
+	osascript -e 'do shell script "java -jar SynBioHub-CLI.jar"'
+fi
